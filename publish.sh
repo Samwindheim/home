e80999d4cf035e9e0004bbb6a9da76e24518abcb
Sam Windheim <sam@windheim.org>
2023-03-12 16:27:24 -0700

    upated publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 # dumb script to force a rebuild that pulls in latest submodule changes
-git commit -m "rebuild site" --allow-empty
-git push origin master
+git submodule update --recursive --remote
+git commit synth -m "update submodules to latest"
+git push
