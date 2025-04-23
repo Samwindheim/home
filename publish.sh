b76323a8b7b0d11a580125c7c3146b2a4be9096b
Sam Windheim <sam@windheim.org>
2025-04-23 12:29:42 +0200

    Prepare to add skippy-bird as submodule

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 # dumb script to force a rebuild that pulls in latest submodule changes
 git submodule update --recursive --remote
-git commit synth -m "update submodules to latest"
+git commit skippy-bird -m "update submodules to latest"
 git push
